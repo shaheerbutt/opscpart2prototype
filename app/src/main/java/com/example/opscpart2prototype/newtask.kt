00394728ec880f3a92f6ed50bdab62d3d041dcb0
shaheerbutt <126863420+shaheerbutt@users.noreply.github.com>
2023-05-07 15:56:50 +0200

    Initial commit

---
+++ app/src/main/java/com/example/opscpart2prototype/newtask.kt
@@ -0,0 +1,11 @@
+package com.example.opscpart2prototype
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class newtask : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_newtask)
+    }
+}
